15c5104c0d34e2f4466be9b1445e122012650671
Masab <Masabk214@gmail.com>
2021-07-26 10:52:58 -0700

    Done my js file

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ let currentHour = currenttime.getHours();
 let currentMinutes = currenttime.getMinutes();
 let currentSeconds = currenttime.getSeconds();
 
+currentMinutes = (currentMinutes<10 ? "0" : "") + currentMinutes;
+currentSeconds = (currentSeconds<10 ? "0" : "") + currentSeconds;
+
 let timeOfDay  = (currenttime < 12) ? "AM":"PM";
+
+currentHour = ( currentHour>12) ? currentHour - 12 : currentHour;
+currentHour = (currentHour==0)? 12 : currentHour;
 
 let currentTimeStr  = currentHour + ":" + currentMinutes  
 + ":" + currentSeconds + " " + timeOfDay;
