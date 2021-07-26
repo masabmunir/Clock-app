232e2097caaf7e0e71830634efc5c180bcc74797
Masab <Masabk214@gmail.com>
2021-07-26 08:22:17 -0700

    Adding basic logic and one function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("working..")
+//console.log("working..")
+
+function updateClock(){
+let currenttime = new Date();
+
+
+let currentHour = currenttime.getHours();
+let currentMinutes = currenttime.getMinutes();
+let currentSeconds = currenttime.getSeconds();
+
+let timeOfDay  = (currenttime < 12) ? "AM":"PM";
+
+let currentTimeStr  = currentHour + ":" + currentMinutes  
++ ":" + currentSeconds + " " + timeOfDay;
+
+document.getElementById('clock').innerHTML = currentTimeStr;
+}
